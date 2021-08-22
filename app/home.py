14b660c251264b211a31aed642349eb7e755f3b1
Felix Bommier <felix.bommier@gmail.com>
2021-08-22 17:26:08 +0200

    Started home page

--- app/home.py
+++ app/home.py
@@ -6,11 +6,21 @@ from . import db
 
 home = Blueprint('home', __name__)
 
-@home.route('/', methods=['GET', 'POST'])
+@home.route('/')
 def index():
-  if request.method == 'POST':
-    search = request.form.get('search')
-    return 'error: ' + search
+  from main import app
 
-  return render_template("home.html")
+  search = request.args.get("search")
+  page = request.args.get("page", 1, type=int)
+  order_by = request.args.get("order-by", "latest")
+  per_page = request.args.get("per-page", app.config['POSTS_PER_PAGE'], type=int)
+
+  if not search:
+    if order_by == "latest":
+      posts = Post.query.order_by(Post.created.desc())
+    elif order_by == "oldest":
+      posts = Post.query.order_by(Post.created.asc())
+    posts = posts.paginate(page, per_page, True)
+
+  return render_template("home.html", posts=posts)
 
